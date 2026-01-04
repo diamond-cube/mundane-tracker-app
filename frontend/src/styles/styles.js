44e98885504612d9f339fe4d70e6171e2b418d41
diamond-cube <96169854+diamond-cube@users.noreply.github.com>
2026-01-04 23:05:27 +0530

    refactor(frontend): new styles file to keep the jsx clean

---
+++ frontend/src/styles/styles.js
@@ -0,0 +1,12 @@
+export const textFieldStyle = {
+  width: "20rem",
+  "& .MuiOutlinedInput-root": {
+    borderRadius: "2rem",
+  },
+};
+
+export const buttonStyle = {
+  textTransform: "none",
+  borderRadius: "2rem",
+  boxShadow: "none",
+};
