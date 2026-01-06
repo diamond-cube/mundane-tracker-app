e3f781a178f2905a06ec70791d9e46c6b535f72e
diamond-cube <96169854+diamond-cube@users.noreply.github.com>
2026-01-07 00:01:53 +0530

    feature(frontend): home dashboard
    Changes:
    
    - Removed the old button-based cards used for navigation
    - Added new cards displaying tracking statistics
    - Added a pie chart to show the distribution of user content

--- frontend/src/views/Home.jsx
+++ frontend/src/views/Home.jsx
@@ -1,13 +1,17 @@
 import {
+  Box,
   Button,
   Card,
+  CardActionArea,
   CardContent,
   CardHeader,
   Grid,
+  LinearProgress,
   Stack,
   Typography,
 } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import Pie from "../components/Graphs/Pie";
 
 export default function Home() {
   const nav = useNavigate();
@@ -18,53 +22,140 @@ export default function Home() {
 
   const cards = [
     {
-      title: "Anime",
-      description: "List of all the animes you've watched",
-      onClick: () => {
-        nav("/anime");
-      },
+      id: 1,
+      title: "Total Tracked",
+      value: 100,
+      color: "primary",
     },
     {
-      title: "Manga",
-      description: "List of all the mangas you've read",
-      onClick: () => {
-        nav("/manga");
-      },
+      id: 2,
+      title: "Completed",
+      value: 50,
+      color: "success",
     },
     {
-      title: "Movies",
-      description: "List of all the movies you've watched",
-      onClick: () => {
-        nav("/movies");
-      },
+      id: 3,
+      title: "Currently Tracking",
+      value: 25,
+      color: "warning",
     },
+    {
+      id: 4,
+      title: "On Hold",
+      value: 25,
+      color: "error",
+    },
+  ];
+  const pieData = [
+    { value: 10, name: "Movies" },
+    { value: 8, name: "Manga" },
+    { value: 3, name: "Anime" },
   ];
   return (
     <>
-      <Grid container justifyContent={"center"} gap={2}>
-        {cards.map((c) => (
-          <Card>
-            <CardHeader
-              title={
-                <span>
-                  <Button
-                    variant="contained"
-                    sx={buttonStyle}
-                    onClick={c.onClick}
-                  >
-                    {c.title}
-                  </Button>
-                </span>
-              }
-            />
-            <CardContent>
-              <Typography variant="body2" color="textSecondary">
-                {c.description}
-              </Typography>
-            </CardContent>
-          </Card>
-        ))}
-      </Grid>
+      <Stack gap={5}>
+        <Grid container justifyContent={"center"} gap={2}>
+          {cards.map((card) => (
+            <Card
+              sx={{
+                width: "18rem",
+                boxShadow: "none",
+                border: "1px solid #e0e0e0",
+                borderRadius: "1rem",
+              }}
+              key={card.id}
+            >
+              <CardActionArea sx={{ height: "100%" }}>
+                <CardContent sx={{ height: "100%" }}>
+                  <Typography variant="body2" color="text.secondary">
+                    {card.title}
+                  </Typography>
+                  <Typography variant="h6" color="text.primary">
+                    {card.value}
+                  </Typography>
+                  <LinearProgress value={card.value} variant="determinate" color={card.color} />
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          ))}
+        </Grid>
+
+        <Stack direction={"row"} justifyContent={"center"} gap={2}>
+          <Box sx={{ width: "100%" }}>
+            <Typography variant="h5" mt={4} mb={2}>
+              My Progress
+            </Typography>
+            <Card
+              sx={{
+                height: "21rem",
+                boxShadow: "none",
+                border: "1px solid #e0e0e0",
+                borderRadius: "1rem",
+              }}
+            >
+              <CardActionArea sx={{ p: 2, height: "100%" }}>
+                <Stack gap={3}>
+                  <Box>
+                    <Typography variant="h6" paddingX={2}>
+                      Movies
+                    </Typography>
+                    <CardContent>
+                      <LinearProgress
+                        variant="determinate"
+                        value={70}
+                        sx={{ height: 10, borderRadius: 5 }}
+                      />
+                    </CardContent>
+                  </Box>
+                  <Box>
+                    <Typography variant="h6" paddingX={2}>
+                      Anime
+                    </Typography>
+                    <CardContent>
+                      <LinearProgress
+                        variant="determinate"
+                        value={50}
+                        sx={{ height: 10, borderRadius: 5 }}
+                        color="success"
+                      />
+                    </CardContent>
+                  </Box>
+                  <Box>
+                    <Typography variant="h6" paddingX={2}>
+                      Manga
+                    </Typography>
+                    <CardContent>
+                      <LinearProgress
+                        variant="determinate"
+                        value={20}
+                        sx={{ height: 10, borderRadius: 5 }}
+                        color="warning"
+                      />
+                    </CardContent>
+                  </Box>
+                </Stack>
+              </CardActionArea>
+            </Card>
+          </Box>
+          <Box sx={{ width: "100%" }}>
+            <Typography variant="h5" mt={4} mb={2}>
+              Library Stats
+            </Typography>
+            <Card
+              sx={{
+                height: "21rem",
+                boxShadow: "none",
+                border: "1px solid #e0e0e0",
+                borderRadius: "1rem",
+              }}
+            >
+              <CardActionArea sx={{ p: 2, height: "100%" }}>
+                <Pie data={pieData} />
+              </CardActionArea>
+            </Card>
+          </Box>
+        </Stack>
+      </Stack>
     </>
   );
 }
